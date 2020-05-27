b9ab545205131133ff7b3ce1b98de41584bb0ac5
Prasoon <39785096+it5prasoon@users.noreply.github.com>
2020-05-27 12:38:11 +0530

    added Password reset via email link!

--- organisehackathon/urls.py
+++ organisehackathon/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
 from main import views
 
 urlpatterns = [
@@ -14,11 +16,21 @@ urlpatterns = [
     path('account/profile/', views.profileView, name='profile'),
     path('account/profile/edit/', views.editProfileView, name='editProfile'),
     path('account/profile/password/', views.changePassword, name='changePassword'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('post/publish/', views.publish, name='publish'),
     path('list/register/', views.register, name='register'),
     path('organiser/', views.organiserView, name='organiserView'),
-    path('send_email/', views.send_email, name='sendEmail')
-
+    path('send_email/', views.send_email, name='sendEmail'),
 ]
 
 if settings.DEBUG:
